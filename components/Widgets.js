4a7d8a3dd2189d0d2ad13a8c01633a72070a25a9
P1zza-L1cht <haruki21corder@gmail.com>
2022-06-29 23:47:43 +0900

    newsapi

--- components/Widgets.js
+++ components/Widgets.js
@@ -1,6 +1,7 @@
 import { SearchIcon } from "@heroicons/react/outline";
+import News from "./News";
 
-export default function Widgets() {
+export default function Widgets({newsResults}) {
   return (
     <div className="xl:w-[600px] hidden lg:inline ml-8 space-y-5">
       <div className="w-[90%] xl:w-[75%] sticky top-0 bg-white py-1.5 z-50">
@@ -12,6 +13,14 @@ export default function Widgets() {
             placeholder="Search Twitter" />
         </div>
       </div>
+
+      <div className="text-grey-700 space-y-3 bg-grey-100 rounded-xl pt-2 w-[90%] xl:w-[75%]">
+        <h4 className="font-bold text-xl px-4">What's happening</h4>
+        {newsResults.map((article) => (
+          <News key={article.title} article={article} />
+        ))}
+        <button className="text-blue-300 ">Show more</button>
+      </div>
     </div>
-  )
+  );
 }
