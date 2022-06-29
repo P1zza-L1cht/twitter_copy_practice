f3210669e11e6ed9fc6983e277cbbc125e5233be
P1zza-L1cht <haruki21corder@gmail.com>
2022-06-29 18:56:30 +0900

    header

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Feed from '../components/Feed'
 import Sidebar from '../components/Sidebar'
 
 
@@ -11,11 +12,12 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className='flex min-h-screen max-w-7xl mx-auto '>
+      <main className='flex min-h-screen mx-auto '>
         {/* Sidebar */}
         <Sidebar/>
 
         {/* feed */}
+        <Feed />
 
         {/* widgets */}
 
