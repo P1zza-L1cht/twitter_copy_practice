48c697dd8dea7c35e2ebf6b6ce1c0a13369a1336
P1zza-L1cht <haruki21corder@gmail.com>
2022-06-29 18:24:38 +0900

    sidebar

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -17,10 +17,10 @@ import {
 
 export default function Sidebar() {
   return (
-    <div>
+    <div className="hidden sm:flex flex-col p-2 xl:items-start fixed h-full">
 
         {/* twitter logo */}
-          <div className=''>
+          <div className='hoverEffect p-0 hover:bg-blue-100 xl:px-1'>
             <Image
               width="50"
               height="50"
@@ -30,8 +30,8 @@ export default function Sidebar() {
           </div>
 
         {/* Menu */}
-        <div className=''>
-          <SidebarMenu  text="Home" Icon={HomeIcon} />
+        <div className='mt-4 mb-2.5 xl:items-start'>
+          <SidebarMenu  text="Home" Icon={HomeIcon} active />
           <SidebarMenu  text="Explore" Icon={HashtagIcon} />
           <SidebarMenu  text="Notifications" Icon={BellIcon} />
           <SidebarMenu  text="Messages" Icon={InboxIcon} />
@@ -43,19 +43,23 @@ export default function Sidebar() {
 
         {/* Button */}
 
-        <button>Tweet</button>
+        <button className='bg-blue-400 text-white rounded-full w-56 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline'>Tweet</button>
 
         {/* Mini-profile */}
 
-        <div className="">
-          {/* <img src="" alt="" /> */}
-          <div className=''>
-            <h4>Haruki Yui</h4>
-            <p>@code.licht</p>
+        <div className=" text-gray-700 flex items-center justify-center xl:justify-start mt-auto">
+          {/* <img 
+            src="" 
+            alt=""
+            className="h-10 w-10 rounded-full"
+          /> */}
+          <div className='leading-5 hidden xl:inline'>
+            <h4 className='font-bold '>Haruki Yui</h4>
+            <p className='text-grey-500'>@code.licht</p>
           </div>
-          <DotsHorizontalIcon  className='h-5' />
+          <DotsHorizontalIcon  className='h-5 xl:ml-8'/>
         </div>
 
     </div>
-  )
+  );
 }
